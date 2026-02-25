b6f7cffc65daf01a353be738e4e0c05c8ba7248e
Luis Silva <siel_alb@hotmail.com>
2026-02-25 17:25:57 -0600

    Initial Spring Boot project setup

---
+++ src/test/java/com/luissilvacoding/listapp/ListappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luissilvacoding.listapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
